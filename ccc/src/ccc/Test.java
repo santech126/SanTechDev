ab5e1502899b6ae4ec604ad7f39411c461e8d0cf
Lenovo <Lenovo@Lenovo-PC>
2018-08-26 23:34:38 +0530

    commented for test

--- ccc/src/ccc/Test.java
+++ ccc/src/ccc/Test.java
@@ -3,7 +3,7 @@ package ccc;
 public class Test {
 public static void main(String[] args) {
 	
-	System.out.println("first programs to be pushed on the git hub repository from eclipse");
+//	System.out.println("first programs to be pushed on the git hub repository from eclipse");
 	
 }
 }
