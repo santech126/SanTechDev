c4fcaaec2b772834971ca166a8c5000fa77e4d1f
santech126 <42718722+santech126@users.noreply.github.com>
2018-09-07 16:35:20 +0530

    Update Test.java

--- ccc/src/ccc/Test.java
+++ ccc/src/ccc/Test.java
@@ -3,10 +3,7 @@ package ccc;
 public class Test {
 public static void main(String[] args) {
 int i=9;
-	if(i==9){
-		
-		
-		
+	if(i==9){	
 	System.out.println("first programs to be pushed on the git hub repository from eclipse");
 }
 }
